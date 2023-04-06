1cd7342305529ea4af1307948b7e94793c2e2c2f
Nathan Lepora <n.lepora@bristol.ac.uk>
2023-04-06 09:50:55 +0100

    partial edits on tactile_sim2real

--- tactile_data/tactile_servo_control.py
+++ tactile_data/tactile_servo_control.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 
 BASE_PATH = os.path.dirname(__file__)
 
---
+++ tactile_data/tactile_sim2real.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_PATH = os.path.dirname(__file__)
+
+BASE_DATA_PATH = os.path.join(BASE_PATH, '..', 'data', 'tactile_sim2real')
+BASE_MODEL_PATH = os.path.join(BASE_PATH, '..', 'models', 'tactile_sim2real')
+BASE_RUNS_PATH = os.path.join(BASE_PATH, '..', 'runs', 'tactile_sim2real')
