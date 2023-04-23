12c060ec5cc2b70c838be9bfa6a17e4c1e08ca6a
Nathan Lepora <n.lepora@bristol.ac.uk>
2023-04-23 17:17:51 +0100

    updated sim_cr_tactip data

--- tactile_data/collect_data/collect_data.py
+++ tactile_data/collect_data/collect_data.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
 
         # setup save dir
         save_dir = os.path.join(BASE_DATA_PATH, data_dir_name)
-        image_dir = os.path.join(save_dir, "images")
+        image_dir = os.path.join(save_dir, "sensor_images")
         make_dir(save_dir)
         make_dir(image_dir)
         save_json_obj(sensor_params, os.path.join(save_dir, 'sensor_params'))
